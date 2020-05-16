dd3ae020031e56bd8a89431884a87c2ca66f7935
jalal Arif <jalalharfjalal@gmail.com>
2020-05-16 23:41:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader)
+
+newHeader.id = 'victory'
+
+
+newHeader.innerHTML = "Jalal is the champion"
+newHeader.color = "orange"
